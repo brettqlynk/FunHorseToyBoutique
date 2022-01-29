79ef73ff96851cd0506254565dec43da6d309599
brettqlynk <brettqlynk@gmail.com>
2022-01-28 16:49:58 -0800

    Revert "Finished cssmodules setup"

--- client/src/component/App.jsx
+++ client/src/component/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import styles from './App.style.css';
 
 const App = () => {
-  return <div className={styles.main}>hello world!</div>;
+  return <div>hello world!</div>;
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -19,20 +19,6 @@ module.exports = {
           },
         },
       },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[name]__[local]___[hash:base64:5]',
-              }
-            }
-          },
-        ]
-      },
     ],
   },
 };
