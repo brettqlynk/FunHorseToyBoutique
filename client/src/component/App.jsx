6707c86ff7b71acc96aa01d9fb7e80adad016502
brettqlynk <brettqlynk@gmail.com>
2022-01-29 11:32:48 -0800

    commented the component reference in App.jsx

--- client/src/component/App.jsx
+++ client/src/component/App.jsx
@@ -3,12 +3,12 @@ import styles from './App.styles.css';
 import MainPage from './Search/MainPage.jsx'
 
 const App = () => {
-//return <div className={styles.main}>hello world!</div>;
-return (
-  <>
-  <MainPage/>
-  </>
-)
+return <div className={styles.main}>hello world!</div>;
+// return (
+//   <>
+//   <MainPage/>
+//   </>
+// )
 };
 
 export default App;
