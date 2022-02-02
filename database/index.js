4ce7c4453ef24bfcb41faf006fb0055b0c1165b6
brettqlynk <brettqlynk@gmail.com>
2022-02-02 09:28:11 -0800

    setting up db

--- database/index.js
+++ database/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const mongoURI = process.env.MONGO_URI;
+const mongoURI = 'mongodb://localhost:27017/blueOcean';
 
-mongoose.connect(mongoURI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 const toySchema = new mongoose.Schema({
   dateCreated: {
